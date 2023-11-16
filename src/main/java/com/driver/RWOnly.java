08e61cb1d5a3b035abfd8de246723d9a6fcad4ea
Pankaj Jha <54464523+pankajjha-04@users.noreply.github.com>
2023-11-16 20:11:04 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+// RWOnly.java
+package com.example;
+
 public class RWOnly {
-    
+    private String name; // Task 2: Private member variable
+
+    // Task 5: Getter and setter functions
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
