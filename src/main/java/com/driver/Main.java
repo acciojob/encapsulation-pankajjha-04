27101a99c882ee5252146d37a61dea6e16d8e2aa
Pankaj Jha <54464523+pankajjha-04@users.noreply.github.com>
2023-11-16 20:10:41 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+// Main.java
+package com.example;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly(); // Task 3: Creating an object of class RWOnly
+
+        // Task 4: Attempting to access private member directly - This will result in a compilation error
+        // System.out.println(obj.name); // This line will generate an error
+
+        // Task 6: Setting value using setter function and accessing using getter function
+        obj.setName("John");
+        System.out.println(obj.getName());
+    }
 }
